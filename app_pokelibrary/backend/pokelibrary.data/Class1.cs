afc37f82eaf8c0819582062b18385e5712c00c93
Joshua Nicholson <jsnicholson08@gmail.com>
2024-05-26 21:37:00 +0100

    initial setup
    setup asp.net backend, react frontend visual studio solution

---
+++ app_pokelibrary/backend/pokelibrary.data/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace pokelibrary.data {
+    public class Class1 {
+
+    }
+}
---
+++ util/index.js
@@ -0,0 +1,37 @@
+const Puppeteer = require('puppeteer')
+const RequestClient = require('request-promise-native');
+
+main();
+
+async function main() {
+    var browser = await Puppeteer.launch({headless:false});
+    const page = await browser.newPage();
+    const result = [];
+    var buildIdentifier;
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+        RequestClient({
+            uri: request.url(),
+            resolveWithFullResponse: true
+        }).then(response => {
+            const request_url = request.url();
+            const request_headers = request.headers();
+            result.push({
+                request_url,
+                request_headers
+            });
+            if(request_url.includes("_next/data")) {
+                buildIdentifier = request_url;
+            }
+            request.continue();
+        }).catch(error => {
+            console.error(error);
+            request.abort();
+        });
+    });
+    await page.goto("https://www.pkmn.gg/series/base/base", {
+        waitUntil: 'networkidle0'
+    });
+    console.log(dataUrl);
+    await browser.close();
+}
